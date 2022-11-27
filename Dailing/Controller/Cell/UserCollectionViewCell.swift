301baa76f678fd3e2b8e1e0f7e78d9c9ffd608e0
Donggaem <ehdrua465111@naver.com>
2022-11-27 17:06:16 +0900

    기본 파일 구조 생성(유저 프로필, 캘린더, 업로드, 지도)

---
+++ Dailing/Controller/Cell/UserCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AddUserCollectionViewCell.swift
+//  Dailing
+//
+//  Created by 김동겸 on 2022/11/27.
+//
+
+import Foundation
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    
+}
